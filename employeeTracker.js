225adfdef65050597d6efbf3cd9bbb6abda247dc
Megan Steblay <megan@steblay.net>
2021-07-06 14:50:30 -0500

    Updating addEmployee to have selectable roles and managers

--- employeeTracker.js
+++ employeeTracker.js
@@ -19,9 +19,8 @@ const connection = mysql.createConnection({
 });
 
 const start = () => {
-    console.log(
-        ``
-    );
+    console.log();
+        
     runSearch();
 };
 
@@ -144,7 +143,7 @@ const addDepartment = () => {
                 (err) => {
                     if (err) throw err;
                     console.log('The department was added successfully!');
-                    
+
                     start();
                 }
             );
@@ -155,108 +154,222 @@ const addRole = () => {
     connection.query(`SELECT * FROM department`, (err, results) => {
         if (err) throw err;
         inquirer
-        .prompt([
-            {
-                name: 'addTitle',
-                type: 'input',
-                message: `What is the role's title?`,
-            },
-            {
-                name: 'addSalary',
-                type: 'input',
-                message: `What is the role's salary?`,
-            },
-            {
-                name: 'addDepartment',
-                type: 'rawlist',
-                choices() {
-                    const choiceArray = [];
-                    results.forEach(({ name }) => {
-                        choiceArray.push(name);
-                    });
-                    return choiceArray;
-                },
-                message: `What department is the role in?`,
-            }
-        ])
-        .then((answer) => {
-            let chosenDepartment;
-            results.forEach((department) => {
-                if (department.name === answer.addDepartment) {
-                    chosenDepartment = department;
-                }
-            });
-
-            connection.query(
-                'INSERT INTO role SET ?',
+            .prompt([
                 {
-                    title: answer.addTitle,
-                    salary: answer.addSalary,
-                    department_id: chosenDepartment.id,
+                    name: 'addTitle',
+                    type: 'input',
+                    message: `What is the role's title?`,
                 },
-                (err) => {
-                    if (err) throw err;
-                    console.log('The role was added successfully!');
-                    
-                    start();
+                {
+                    name: 'addSalary',
+                    type: 'input',
+                    message: `What is the role's salary?`,
+                },
+                {
+                    name: 'addDepartment',
+                    type: 'rawlist',
+                    choices() {
+                        const choiceArray = [];
+                        results.forEach(({ name }) => {
+                            choiceArray.push(name);
+                        });
+                        return choiceArray;
+                    },
+                    message: `What department is the role in?`,
                 }
-            );
-        });
+            ])
+            .then((answer) => {
+                let chosenDepartment;
+                results.forEach((department) => {
+                    if (department.name === answer.addDepartment) {
+                        chosenDepartment = department;
+                    }
+                });
+
+                connection.query(
+                    'INSERT INTO role SET ?',
+                    {
+                        title: answer.addTitle,
+                        salary: answer.addSalary,
+                        department_id: chosenDepartment.id,
+                    },
+                    (err) => {
+                        if (err) throw err;
+                        console.log('The role was added successfully!');
+
+                        start();
+                    }
+                );
+            });
     })
 };
 
 const addEmployee = () => {
     connection.query(`SELECT * FROM role`, (err, results) => {
         if (err) throw err;
-        inquirer
-        .prompt([
-            {
-                name: 'addFirst',
-                type: 'input',
-                message: `What is the employee's first name?`,
-            },
-            {
-                name: 'addLast',
-                type: 'input',
-                message: `What is the employee's last name?`,
-            },
-            {
-                name: 'addRole',
-                type: 'rawlist',
-                choices() {
-                    const choiceArray = [];
-                    results.forEach(({ title }) => {
-                        choiceArray.push(title);
+        connection.query(`SELECT * FROM employee`, (err, results2) => {
+            if (err) throw err;
+            console.log(results);
+            inquirer
+                .prompt([
+                    {
+                        name: 'addFirst',
+                        type: 'input',
+                        message: `What is the employee's first name?`,
+                    },
+                    {
+                        name: 'addLast',
+                        type: 'input',
+                        message: `What is the employee's last name?`,
+                    },
+                    {
+                        name: 'addRole',
+                        type: 'rawlist',
+                        choices() {
+                            const choiceArray = [];
+                            results.forEach(({ title }) => {
+                                choiceArray.push(title);
+                            });
+                            return choiceArray;
+                        },
+                        message: `What role does the employee have?`,
+                    },
+                    {
+                        name: 'addManager',
+                        type: 'rawlist',
+                        choices() {
+                            const choiceArray2 = [];
+                            results2.forEach(({ first_name, last_name }) => {
+                                let fullName = first_name + " " + last_name;
+                                choiceArray2.push(fullName);
+                            });
+                            return choiceArray2;
+                        },
+                        message: `Who is the employee's manager?`,
+                    }
+                ])
+                .then((answer) => {
+                    let chosenRole;
+                    results.forEach((role) => {
+                        if (role.title === answer.addRole) {
+                            chosenRole = role;
+                        }
                     });
-                    return choiceArray;
-                },
-                message: `What role does the employee have?`,
-            }
-        ])
-        .then((answer) => {
-            let chosenRole;
-            results.forEach((role) => {
-                if (role.title === answer.addRole) {
-                    chosenRole = role;
-                }
-            });
+    
+                    let chosenManager;
+                    results2.forEach((employee) => {
+                        if ((employee.first_name + " " + employee.last_name) === answer.addManager) {
+                            chosenManager = employee;
+                        }
+                    });
+    
+                    connection.query(
+                        'INSERT INTO employee SET ?',
+                        {
+                            first_name: answer.addFirst,
+                            last_name: answer.addLast,
+                            role_id: chosenRole.id,
+                            manager_id: chosenManager.id,
+                        },
+                        (err) => {
+                            if (err) throw err;
+                            console.log('The employee was added successfully!');
+    
+                            start();
+                        }
+                    );
+                });
+            })
+        })
+};
 
-            connection.query(
-                'INSERT INTO employee SET ?',
-                {
-                    first_name: answer.addFirst,
-                    last_name: answer.addLast,
-                    role_id: chosenRole.id,
-                },
-                (err) => {
-                    if (err) throw err;
-                    console.log('The employee was added successfully!');
-                    
-                    start();
-                }
-            );
-        });
-    })
+// AGAIN, FIRST AND LAST NAME PROBLEMS
+// const updateERole = () => {
+//     connection.query('SELECT * FROM employee', (err, results) => {
+//         if (err) throw err;
+//         inquirer
+//             .prompt([
+//                 {
+//                     name: 'upEmployee',
+//                     type: 'rawlist',
+//                     choices() {
+//                         const choiceArray = [];
+//                         results.forEach(({ first_name }) => {
+//                             choiceArray.push(first_name);
+//                         });
+//                         return choiceArray;
+//                     },
+//                     message: `Which employee's role would you like to update?`,
+//                 },
+//                 {
+//                     name: 'upRole',
+//                     type: 'rawlist',
+//                     choices() {
+//                         const choiceArray = [];
+//                         results.forEach(({ role }) => {
+//                             choiceArray.push(role);
+//                         });
+//                         return choiceArray;
+//                     },
+//                     message: `Which employee's role would you like to update?`,
+//                 },
+//             ])
+//             .then((answer) => {
+//                 let chosenEmployee;
+//                 results.forEach((employee) => {
+//                     if (item.item_name === answer.choice) {
+//                         chosenItem = item;
+//                     }
+//                 });
+
+//                 connection.query(
+//                     'UPDATE auctions SET ? WHERE ?',
+//                     [
+//                         {
+//                             highest_bid: answer.bid,
+//                         },
+//                         {
+//                             id: chosenItem.id,
+//                         },
+//                     ],
+//                     (error) => {
+//                         if (error) throw err;
+//                         console.log('Bid placed successfully!');
+//                         start();
+//                     }
+//                 );
+//             } else {
+//                 // bid wasn't high enough, so apologize and start over
+//                 console.log('Your bid was too low. Try again...');
+//                 start();
+//             }
+//         });
+// });
+
+// };
+
+const updateEManager = () => {
+
+};
+
+const viewMEmployees = () => {
+
+};
+
+const deleteDepartments = () => {
+
+};
+
+const deleteRoles = () => {
+
+};
+
+const deleteEmployees = () => {
+
+};
+
+const viewDepartmentBudget = () => {
+
 };
 
 
@@ -271,19 +384,19 @@ connection.connect((err) => {
 });
 
 
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-// |
-// |    _____
-// |   | ____|
-// |   |  _| |
-// |   | |___|
-// |   |_____|
-// |
-// |    __  __
-// |   |  \/  |
-// |   | |\/| |
-// |   | |  | |
-// |   |_|  |_|
-// |
-// |
-// | _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// |                                                     |
+// |    _____                 _                          |
+// |   | ____|_ __ ___  _ __ | | ___  _   _  ___  ___    |
+// |   |  _| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \   |
+// |   | |___| | | | | | |_) | | (_) | |_| |  __/  __/   |
+// |   |_____|_| |_| |_| .__/|_|\___/ \__, |\___|\___|   |
+// |                   |_|            |___/              |
+// |    __  __                                           |
+// |   |  \/  | __ _ _ __   __ _  __ _  ___ _ __         |
+// |   | |\/| |/ _` | `_ \ / _` |/ _` |/ _ \ `__|        |
+// |   | |  | | (_| | | | | (_| | (_| |  __/ |           |
+// |   |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|           |
+// |                             |___/                   |
+// |                                                     |
+// | _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
